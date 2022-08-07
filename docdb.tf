fc2e8a88cde7921bb0c9cd90e2ec2cc92b96cbfe
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-06 19:06:04 -0700

    updating ./terraform/tf-module-docdb

--- docdb.tf
+++ docdb.tf
@@ -1,12 +1,12 @@
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${local.TAG_PREFIX}-docdb"
-  engine                  = var.ENGINE
-  engine_version          = var.ENGINE_VERSION
-  master_username         = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]
-  master_password         = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]
-  vpc_security_group_ids  = [aws_security_group.main.id]
-  db_subnet_group_name    = aws_docdb_subnet_group.main.name
-  skip_final_snapshot     = true
+  cluster_identifier              = "${local.TAG_PREFIX}-docdb"
+  engine                          = var.ENGINE
+  engine_version                  = var.ENGINE_VERSION
+  master_username                 = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]
+  master_password                 = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]
+  vpc_security_group_ids          = [aws_security_group.main.id]
+  db_subnet_group_name            = aws_docdb_subnet_group.main.name
+  skip_final_snapshot             = true
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
 }
 
