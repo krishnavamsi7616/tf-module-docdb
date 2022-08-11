518c3d6480bf69cc6cac36a2045e5cf8457872db
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-11 00:41:48 -0700

    updating ./terraform/tf-module-docdb

--- schema.tf
+++ schema.tf
@@ -4,10 +4,9 @@ resource "null_resource" "schema" {
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
 unzip -o mongodb.zip
-cd mongodb.zip
 cd mongodb-main
-mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --username jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"] --password jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"] --sslCAFile /home/centos/rds-combined-ca-bundle.pem <catalogue.js
-mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --username jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"] --password jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"] --sslCAFile /home/centos/rds-combined-ca-bundle.pem <users.js
+mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]} --sslCAFile /home/centos/rds-combined-ca-bundle.pem <catalogue.js
+mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]} --sslCAFile /home/centos/rds-combined-ca-bundle.pem <users.js
 EOF
 }
 }
